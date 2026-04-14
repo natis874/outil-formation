691c28af08cef761d52a69a3e6fe9c117c606be0
natisenissay <yassine.tmnt@gmail.com>
2026-04-14 15:38:01 +0200

    Create CategoryDTO.java

---
+++ src/main/java/org/tamyass/outilformation/dto/CategoryDTO.java
@@ -0,0 +1,13 @@
+package org.tamyass.outilformation.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class CategoryDTO {
+    private Long id;
+    private Long name;
+}
