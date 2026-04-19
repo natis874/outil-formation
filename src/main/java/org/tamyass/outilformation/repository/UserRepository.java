1f489c740d601ec4a4599970ce53d2535b5396c4
Noteoh <ruphut@gmail.com>
2026-04-19 21:36:15 +0200

    Add annotation Repository

--- src/main/java/org/tamyass/outilformation/repository/UserRepository.java
+++ src/main/java/org/tamyass/outilformation/repository/UserRepository.java
@@ -1,7 +1,8 @@
 package org.tamyass.outilformation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.tamyass.outilformation.entities.User;
-
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 }
