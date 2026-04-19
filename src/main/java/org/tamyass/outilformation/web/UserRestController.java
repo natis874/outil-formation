e6a8f5e2cc2a277bf94a97bd928389bd4682db53
Noteoh <ruphut@gmail.com>
2026-04-19 21:36:33 +0200

    Delete unused import

--- src/main/java/org/tamyass/outilformation/web/UserRestController.java
+++ src/main/java/org/tamyass/outilformation/web/UserRestController.java
@@ -1,14 +1,11 @@
 package org.tamyass.outilformation.web;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.tamyass.outilformation.dto.UserDTO;
-import org.tamyass.outilformation.entities.User;
 import org.tamyass.outilformation.service.UserService;
 
-import java.net.Authenticator;
 import java.net.URI;
 import java.util.List;
 
