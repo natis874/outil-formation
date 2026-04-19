f87b708560b0f6d8a3f3c12cd4c78711fe778ff2
Noteoh <ruphut@gmail.com>
2026-04-19 21:34:15 +0200

    Add repository annnotation

--- src/main/java/org/tamyass/outilformation/repository/AssessmentQuestionRepository.java
+++ src/main/java/org/tamyass/outilformation/repository/AssessmentQuestionRepository.java
@@ -1,7 +1,8 @@
 package org.tamyass.outilformation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.tamyass.outilformation.entities.AssessmentQuestion;
-
+@Repository
 public interface AssessmentQuestionRepository extends JpaRepository<AssessmentQuestion,Long> {
 }
