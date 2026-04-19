dcb4f9479db7c9b3b8adf9b0ad54b9922a5eab7b
Noteoh <ruphut@gmail.com>
2026-04-19 21:34:19 +0200

    Add repository annnotation

--- src/main/java/org/tamyass/outilformation/repository/CategoryRepository.java
+++ src/main/java/org/tamyass/outilformation/repository/CategoryRepository.java
@@ -1,7 +1,8 @@
 package org.tamyass.outilformation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.tamyass.outilformation.entities.Category;
-
+@Repository
 public interface CategoryRepository extends JpaRepository<Category,Long> {
 }
