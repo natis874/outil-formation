0c17776951f45da6f48d17ad2e98127b4aeea412
Noteoh <ruphut@gmail.com>
2026-04-19 21:35:51 +0200

    Create SkillService

--- src/main/java/org/tamyass/outilformation/service/SkillService.java
+++ src/main/java/org/tamyass/outilformation/service/SkillService.java
@@ -1,6 +1,16 @@
 package org.tamyass.outilformation.service;
 
+import org.tamyass.outilformation.dto.SkillDTO;
+import org.tamyass.outilformation.dto.UserDTO;
+
+import java.util.List;
+
 public interface SkillService {
 
+    List<SkillDTO> getSkillsbyUserId(Long userId);
+
+    SkillDTO UpdateEstimation(Long skillId, Integer level);
+
+    List<SkillDTO> getSkillByCategoryId(Long categoryId);
 
 }
