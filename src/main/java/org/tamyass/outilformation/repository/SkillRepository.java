1fd962b70c4ce04a7854ee00a69ce18bdf461266
Noteoh <ruphut@gmail.com>
2026-04-19 21:34:56 +0200

    Add repository annnotation

--- src/main/java/org/tamyass/outilformation/repository/SkillRepository.java
+++ src/main/java/org/tamyass/outilformation/repository/SkillRepository.java
@@ -1,7 +1,15 @@
 package org.tamyass.outilformation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.tamyass.outilformation.entities.Skill;
 
+import java.util.List;
+
+@Repository
 public interface SkillRepository extends JpaRepository<Skill,Long> {
+
+    List<Skill> findByUserId(Long userId);
+    List<Skill> findByCategoryId(Long categoryId);
+
 }
