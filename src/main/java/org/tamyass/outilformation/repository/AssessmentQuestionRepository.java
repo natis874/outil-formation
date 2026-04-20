c9ab60ec69466aa8f344da30c30a86e1dcac2134
natisenissay <yassine.tmnt@gmail.com>
2026-04-20 18:18:13 +0200

    Update AssessmentQuestionRepository.java

--- src/main/java/org/tamyass/outilformation/repository/AssessmentQuestionRepository.java
+++ src/main/java/org/tamyass/outilformation/repository/AssessmentQuestionRepository.java
@@ -3,6 +3,13 @@ package org.tamyass.outilformation.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.tamyass.outilformation.entities.AssessmentQuestion;
+
+import java.util.List;
+
 @Repository
 public interface AssessmentQuestionRepository extends JpaRepository<AssessmentQuestion,Long> {
+
+    List<AssessmentQuestion> findAssessmentQuestionBySkillId(Long skillId);
+    void deleteAssessmentQuestionBySkillId(Long skillId);
+
 }
