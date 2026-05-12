1456ba77fbc5bad64b157f02b0cac33c3d952b5e
natisenissay <yassine.tmnt@gmail.com>
2026-05-13 00:45:15 +0200

    Add Optional in the repository

--- src/main/java/org/tamyass/outilformation/repository/CategoryRepository.java
+++ src/main/java/org/tamyass/outilformation/repository/CategoryRepository.java
@@ -3,6 +3,11 @@ package org.tamyass.outilformation.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.tamyass.outilformation.entities.Category;
+
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category,Long> {
+
+    Optional<Category> findByName(String name);
 }
