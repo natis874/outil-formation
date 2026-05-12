cab3db5dc44effa438196b86c7603e062073fdcc
natisenissay <yassine.tmnt@gmail.com>
2026-05-13 00:44:32 +0200

    Create CVRestContoller.java

---
+++ src/main/java/org/tamyass/outilformation/web/CVRestContoller.java
@@ -0,0 +1,29 @@
+package org.tamyass.outilformation.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.tamyass.outilformation.dto.SkillDTO;
+import org.tamyass.outilformation.dto.cv.CVResponseDTO;
+import org.tamyass.outilformation.dto.cv.CVSkillDTO;
+import org.tamyass.outilformation.service.CVService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cv")
+@RequiredArgsConstructor // pour faire les contructeurs des services
+public class CVRestContoller {
+    private final CVService cvService;
+
+    public ResponseEntity<CVResponseDTO> extractSkills(@RequestParam("file") MultipartFile file){
+        return ResponseEntity.ok(cvService.extractSkillFromCV(file));
+    }
+    public ResponseEntity<List<SkillDTO>> saveSkills(
+            @PathVariable Long userId,
+            @RequestBody List<CVSkillDTO> skills
+    ){
+        return ResponseEntity.ok(cvService.saveSkills(userId,skills));
+    }
+}
